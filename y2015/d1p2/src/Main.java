eb95f3942063bcd7a7af565e4ed1913795eea257
Akshat Singh <twisted.nematic57@gmail.com>
2025-07-11 23:52:56 -0700

    Done with 2015 Day 1 Part 2

---
+++ y2015/d1p2/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("CWD = " + System.getProperty("user.dir") + "\n\n");
+		String filePath = "input.txt";
+		try (BufferedReader f = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
+			int ch;
+			int floor = 0, step = 0;
+			while ((ch = f.read()) != -1) {
+				step++;
+				if((char)ch == '(') {
+					floor++;
+				} else {
+					floor--;
+				}
+				System.out.println("Step = " + step + "; Current floor = " + String.format("% d", floor));
+				
+				if(floor == -1) {
+					break;
+				}
+			}
+			
+			System.out.println("\nInstruction # that enters basement = " + step);
+		} catch (IOException e) {
+			System.out.println("\n" + e.getMessage());
+			System.out.println("\n\n FATAL ERROR: The input file was not found.");
+		}
+	}
+}
