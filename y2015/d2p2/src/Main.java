776b7284e656ce965c4b76bfa5c1670d9fe29ca8
Akshat Singh <twisted.nematic57@gmail.com>
2025-07-13 00:08:26 -0700

    Done with 2015 Day 2 Part 2; changed naming scheme a little bit + global indentation settings

--- y2015/d2p2/src/Main.java
+++ y2015/d2p2/src/Main.java
@@ -9,13 +9,13 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("CWD = " + System.getProperty("user.dir") + "\n\n");
 		String filePath = "input.txt";
-		try (BufferedReader f = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
+		try (BufferedReader inputFile = Files.newBufferedReader(Paths.get(filePath), StandardCharsets.UTF_8)) {
 			String currLine;
 			int[] dimensions = new int[3];
-			int[] areas = new int[3];
+			int[] perimeters = new int[3];
 			int step = 0;
 			long total = 0;
-			while ((currLine = f.readLine()) != null) {
+			while ((currLine = inputFile.readLine()) != null) {
 				step++;
 
 				String[] dimensions_str = currLine.split("x"); // Extract dimensions from string
@@ -23,20 +23,17 @@ public class Main {
 				dimensions[1] = Integer.parseInt(dimensions_str[1]); // Width
 				dimensions[2] = Integer.parseInt(dimensions_str[2]); // Height
 
-				areas[0] = dimensions[0]*dimensions[1];
-				areas[1] = dimensions[1]*dimensions[2];
-				areas[2] = dimensions[2]*dimensions[0];
-				Arrays.sort(areas); // The "slack" (area of smallest side)
+				perimeters[0] = 2*dimensions[0]+2*dimensions[1];
+				perimeters[1] = 2*dimensions[1]+2*dimensions[2];
+				perimeters[2] = 2*dimensions[2]+2*dimensions[0];
+				Arrays.sort(perimeters); // First element will be the smallest perimeter
 
-				total += (2L*dimensions[0]*dimensions[1] + // -----------------------
-									2L*dimensions[1]*dimensions[2] + // Surface area calculation (2*l*w + 2*w*h + 2*h*l)
-									2L*dimensions[2]*dimensions[0] + // -----------------------
-									areas[0]); // Gotta add the slack too!
+				total += perimeters[0] + (long)dimensions[0]*dimensions[1]*dimensions[2]; // Add smallest perimeter + volume (for the bow)
 
 				System.out.println("Step: " + step + "; Total: " + total);
 			}
 
-			System.out.println("\nGrand total wrapping paper needed: " + total);
+			System.out.println("\nGrand total feet of ribbon needed: " + total);
 		} catch (IOException e) {
 			System.out.println("\n" + e.getMessage());
 			System.out.println("\nFATAL ERROR: The input file was not found.");
